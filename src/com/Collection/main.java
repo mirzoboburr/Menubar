d761e94c77e2682a22479463e6172d12a7f6d407
Bobur <bobur927788575@outlook.com>
2022-05-04 21:56:14 +0500

    Menubar
    Just download exe file

---
+++ src/com/Collection/main.java
@@ -0,0 +1,15 @@
+package com.Collection;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Main{
+
+    public static void main(String[] args) {
+
+        new MyFrame();
+    }
+}
+
+
+
